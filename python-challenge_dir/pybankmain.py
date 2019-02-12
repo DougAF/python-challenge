82a42d1480bcc470c392bd24435140c96f01f8bf
DougAF <31972099+DougAF@users.noreply.github.com>
2019-02-11 19:31:21 -0600

    Day 1

--- python-challenge_dir/pybankmain.py
+++ python-challenge_dir/pybankmain.py
@@ -3,8 +3,15 @@
 # This will allow us to create file paths across operating systems
 import os
 import csv
+#vars
 
+months = 0
+money = 0
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# lambda arguments : expression
+#diff = lambda x, y : y - x
+
 #1.1 Read file
 # Method 2: Improved Reading using CSV module
 
@@ -12,19 +19,42 @@ with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    next(csvreader) #skips header
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
-#1.2 Calculate Total Months
-#1.3 Sum of all Profits/Losses
+        months += 1 #1.2 Calculate Total Months
+        money += int(row[1]) #1.3 Sum of all Profits/Losses
 #1.4 Avg change in profits/losses
-#1.5 Max inc in Profit (best day)
+avg = int(money/months)
+#1.5 Max inc in Profit (best day) I BELIEVE i need to create a list [month, value] to search for max and mins and print.max(value) worst case i can have it print the values directly
+
 #1.6 Max Decrease in losses (worst day)
+
 #1.7 Output to terminal and CSV
+print('This data was accumulated over ' + str(months)  + " months.")
+print('The company made ' + '$' + str(money) + ' over that period.')
+print('This company averaged a profit of ' + '$' + str(avg) + ' each month.')
+    
+    # Specify the file to write to --> write to csv
+output_path = os.path.join("Resources", "newbudget_data.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfileout:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfileout, delimiter=',')
+
+    # Write the first row (column headers)
+    csvwriter.writerow(['Output', 'Value'])
+
+    # Write the Third  row
+    csvwriter.writerow(['Total Months ', months])
+    # Write the Fourth row
+    csvwriter.writerow(['Total Revenue ', money])
+    # Write the second row
+    csvwriter.writerow(['Average Change ', "1"])
+
+    csvwriter.writerow(['Best Month' , ("Date " + "income")])
+    
+    csvwriter.writerow(['Worst Month' , ("Date " + "income")])
--- python-challenge_dir/pypollmain.py
+++ python-challenge_dir/pypollmain.py
@@ -4,6 +4,7 @@
 import os
 import csv
 
+
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 #2.1 Read csv
@@ -12,6 +13,7 @@ with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader) #skips header
 
     print(csvreader)
 
@@ -25,7 +27,26 @@ with open(csvpath, newline='') as csvfile:
 #2.2 Analyze vote and calc Total
 
 #2.3 List Unique Candidates
+
 #2.4 % of votes each Candidate won Can_per = (Can_vote/Total_vote)100
+
 #2.5 Total # votes each candidate Can_per x Total Vote or sum by tick
+
 #2.6 Candidate with most votes (print max)
+
 #2.7 Outputs to toerminal and to CSV
+    #Write to csv# Specify the file to write to
+output_path = os.path.join("..", "output", "new.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row (column headers)
+    csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
+
+    # Write the second row
+    csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+
